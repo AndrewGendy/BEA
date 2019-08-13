3f0f676a377a2c9f75202776c90c8dac122a2401
AndrewGendy <mando201025@yahoo.com>
2019-08-13 08:54:08 -0400

    Added map button

--- app/src/main/java/com/example/BEA/MainActivity.java
+++ app/src/main/java/com/example/BEA/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +12,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
         TextView tvBEA1,tvBEA2,tvADP;
-        Button btnADP;
+        Button btnADP, btnMaps;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,22 @@ public class MainActivity extends AppCompatActivity {
                 tvBEA2 = findViewById(R.id.tvBEA2);
                 tvADP = findViewById(R.id.tvADP);
                 btnADP = findViewById(R.id.btnADP);
+                btnMaps = findViewById(R.id.btnMaps);
 
 
                 btnADP.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://my.adp.com"));
+                                startActivity(intent);
+                        }
+                });
 
+                btnMaps.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=901 Marcon Blvd, Allentown, PA 18109"));
+                                startActivity(intent);
                         }
                 });
 
